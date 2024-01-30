3f62fee7b4f2b4f0a15d39be8034a0097d32411a
JuliGasca <j.gasca@uniandes.edu.com>
2024-01-29 19:20:20 -0500

    “Entrega Final – laboratorio 1”

--- App/view.py
+++ App/view.py
@@ -52,6 +52,8 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Mods Lab 1, agregar la opcion 3.
+    print("3- Cargar Libros con Tags")
+
 
     print("0- Salir")
 
@@ -96,7 +98,7 @@ if __name__ == "__main__":
     while working:
         printMenu()
         inputs = input("Seleccione una opción para continuar\n")
-        if int(inputs[0]) == 1:
+        if int(inputs[0]) ==1 1:
             print("Cargando información de libros....")
             books = loadBooks(control)
             print("Total de libros cargados: " + str(books) + "\n")
@@ -107,6 +109,11 @@ if __name__ == "__main__":
             print("Total de tags cargados: " + str(tags) + "\n")
 
         # TODO: Mods Lab 1, agregar la funcion opt 3 -> ladBookTags().
+        elif int(inputs[0]) == 3:
+            print("Cargando información de libros con tags…")
+            book_tags = loadBooksTags(control)
+            print("Total de libros con tags cargados: " + str(book_tags))
+
         elif int(inputs[0]) == 3:
             pass
 
